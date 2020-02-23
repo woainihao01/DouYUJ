01ea55c142703b8a90934d28fb7f44217d04c04f
apple <ruixin_jia@126.com>
2020-02-23 15:25:17 +0800

    添加redView

---
+++ DYZB/DYZB/redView.swift
@@ -0,0 +1,21 @@
+//
+//  redView.swift
+//  DYZB
+//
+//  Created by ruixin_jia on 2020/2/23.
+//  Copyright © 2020 ruixin_jia. All rights reserved.
+//
+
+import UIKit
+
+class redView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
